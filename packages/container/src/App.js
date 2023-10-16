3a84bfca20e35bb3aa12921fabd0c52c5f76cfeb
kansinghraghav <kansinghraghav@gmail.com>
2023-10-16 13:44:30 +0530

    Initial Commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+//console.log(mount);
+
+export default () => {
+    return <div>
+       <h1>Hi kan !</h1>
+        <hr />
+        <MarketingApp />
+    </div>
+}
