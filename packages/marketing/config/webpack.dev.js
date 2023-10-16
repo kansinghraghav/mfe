8f1ee25facf632e39f958d40689953c344290189
kansinghraghav <kansinghraghav@gmail.com>
2023-10-16 16:58:24 +0530

    created container deploy script

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -21,10 +21,7 @@ const devConfig = {
                 './MarketingApp': './src/bootstrap'
             },
             shared: packageJson.dependencies
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
+        })       
     ]
 };
 
