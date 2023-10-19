b7842892077872ac9dd720f63cd3aa59be74e290
kansinghraghav <kansinghraghav@gmail.com>
2023-10-19 16:30:38 +0530

    code formating

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -6,14 +6,16 @@ import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
 export default () => {
-    return <div>
-        <StylesProvider>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/pricing" component={Pricing} />
-                    <Route exact path="/" component={Landing} />
-                </Switch>
-            </BrowserRouter>
-        </StylesProvider>
+  return (
+    <div>
+      <StylesProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
     </div>
+  );
 };
