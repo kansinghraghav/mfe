c156cb0d12ef99ebee34d36b1dd2c224431c2f2f
kansinghraghav <kansinghraghav@gmail.com>
2023-10-25 12:12:59 +0530

    container app js fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,13 +4,19 @@ import MarketingApp from './components/MarketingApp';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
+
 export default () => {
-  return (
+  return (   
     <BrowserRouter>
-    <div>      
-      <Header />
-      <MarketingApp />
-    </div>
+     <StylesProvider generateClassName={generateClassName}>
+      <div>      
+        <Header />
+        <MarketingApp />
+      </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
